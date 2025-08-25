b84472503def1fb2a5bbd7bc3f2b7b7c2aeab734
Guilherme Alves Pedroso <alvespedrosog@gmail.com>
2025-08-25 08:17:49 -0300

    Implement beforeEach

--- src/test/java/ApDexTeste.java
+++ src/test/java/ApDexTeste.java
@@ -1,17 +1,26 @@
 import org.example.ApDex;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ApDexTeste {
     // Guilherme Alves Pedroso
     // RM: 555357
 
+    private ApDex apDex;
+    private float amostras;
+
+    @BeforeEach
+    void setUp() {
+        // arrange
+        apDex = new ApDex();
+        amostras = 555357;
+    }
+
     @Test
     void validarApDexExcelente(){
-        // arrange
-        ApDex apDex = new ApDex();
         // act
-        float score = apDex.calcularApDex(555357, 0, 555357);
+        float score = apDex.calcularApDex(555357, 0, amostras);
         // assert
         Assertions.assertEquals(1.0, score, 0.0001);
     }
@@ -19,11 +28,10 @@ public class ApDexTeste {
     @Test
     void validarApDexBom() {
         // arrange
-        ApDex apDex = new ApDex();
         float satisfatorio = 0.90f * 555357;
         float toleravel = 0;
         // act
-        float score = apDex.calcularApDex(satisfatorio, toleravel, 555357);
+        float score = apDex.calcularApDex(satisfatorio, toleravel, amostras);
         // assert
         Assertions.assertEquals(0.90, score, 0.0001);
     }
@@ -31,11 +39,10 @@ public class ApDexTeste {
     @Test
     void validarApDexRazoavel() {
         // arrange
-        ApDex apDex = new ApDex();
         float satisfatorio = 0.50f * 555357;
         float toleravel = 0.50f * 555357;
         // act
-        float score = apDex.calcularApDex(satisfatorio, toleravel, 555357);
+        float score = apDex.calcularApDex(satisfatorio, toleravel, amostras);
         // assert
         Assertions.assertEquals(0.75, score, 0.0001);
     }
@@ -43,11 +50,10 @@ public class ApDexTeste {
     @Test
     void validarApDexRuim() {
         // arrange
-        ApDex apDex = new ApDex();
         float satisfatorio = 0.40f * 555357;
         float toleravel = 0.30f * 555357;
         // act
-        float score = apDex.calcularApDex(satisfatorio, toleravel, 555357);
+        float score = apDex.calcularApDex(satisfatorio, toleravel, amostras);
         // assert
         Assertions.assertEquals(0.55, score, 0.0001);
     }
@@ -55,11 +61,10 @@ public class ApDexTeste {
     @Test
     void validarApDexInaceitavel() {
         // arrange
-        ApDex apDex = new ApDex();
         float satisfatorio = 0.10f * 555357;
         float toleravel = 0.20f * 555357;
         // act
-        float score = apDex.calcularApDex(satisfatorio, toleravel, 555357);
+        float score = apDex.calcularApDex(satisfatorio, toleravel, amostras);
         // assert
         Assertions.assertEquals(0.20, score, 0.0001);
     }
